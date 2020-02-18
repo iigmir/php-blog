c61e1f2c5f3663b46fb13dab0f81ac4dfaa35960
iigmir <roc120j@gmail.com>
2020-02-18 16:27:52 +0800

    Add infomations

--- src/api/joke/index.php
+++ src/api/joke/index.php
@@ -1,14 +1,11 @@
 <?php
-// Origin header
 $request_origin = isset( $_SERVER["SERVER_NAME"] ) ? $_SERVER["SERVER_NAME"] : null;
 $acceptable_origin = [
-    "localhost",
-    "127.0.0.1",
-    "iigmir.github.io",
-    "iismmx.000webhostapp.com",
+    "localhost", "127.0.0.1",
+    "iigmir.github.io", "iismmx.000webhostapp.com",
 ];
-$data = array();
 $allow_credentials = in_array( $request_origin, $acceptable_origin );
+$data = array();
 
 if ( $allow_credentials )
 {
@@ -22,7 +19,10 @@ if ( $allow_credentials )
     $response_data = curl_exec($ch);
     if (!curl_errno($ch))
     {
-        $data = json_decode( $response_data );
+        $id = json_decode( $response_data )->id;
+        $data["API"] = json_decode( $response_data );
+        $data["URL"] = "https://icanhazdadjoke.com/j/${id}";
+        $data["SRC"] = "https://icanhazdadjoke.com/";
     }
     curl_close($ch);
 }
