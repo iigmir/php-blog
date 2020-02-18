0f55a0d6fdf8da7d4f311838d86ad5bb3620888d
iigmir <roc120j@gmail.com>
2020-02-18 16:16:23 +0800

    A joke api

--- src/api/joke/index.php
+++ src/api/joke/index.php
@@ -1,4 +1,5 @@
 <?php
+// Origin header
 $request_origin = isset( $_SERVER["SERVER_NAME"] ) ? $_SERVER["SERVER_NAME"] : null;
 $acceptable_origin = [
     "localhost",
@@ -6,17 +7,24 @@ $acceptable_origin = [
     "iigmir.github.io",
     "iismmx.000webhostapp.com",
 ];
-$data = array(
-    "accepted" => in_array( $request_origin, $acceptable_origin ),
-    "origin" => $request_origin,
-);
+$data = array();
 $allow_credentials = in_array( $request_origin, $acceptable_origin );
 
 if ( $allow_credentials )
 {
+    // Origin header
     header("Access-Control-Allow-Origin: ${request_origin}");
     header("Access-Control-Allow-Credentials: true");
+    // Joke API
+    $ch = curl_init( "https://icanhazdadjoke.com" );
+    curl_setopt( $ch, CURLOPT_HTTPHEADER, [ "Accept: application/json" ] );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+    $response_data = curl_exec($ch);
+    if (!curl_errno($ch))
+    {
+        $data = json_decode( $response_data );
+    }
+    curl_close($ch);
 }
-
 header("Content-Type: application/json");
 echo json_encode( $data );
